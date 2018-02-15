5f63e89e6580405fa042826703c386bd13e36a15
indykab <pcccarcenter@gmail.com>
2018-02-15 14:48:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'bc9e1986336a7ce9bbe7574e7f16daf1';
+$access_token = '0qp9Q66O8W1tKVR0I4D7yojI+8Oh6ZoXViSsQ/V9m+m0qg86NkRXnRBohaaaaqzGfCcOzek+AKbecKEYCs/S4zBY9sUDwhLuEsGsF1c5VRyM67RwBKsHI5mjS7IAztPvfhyYa/OG7gl/plFl8uoWBgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
